367f3db3f354e5e4d09e66df1961aa4e6b35a46b
UlisesStamatio <97748427+UlisesStamatio@users.noreply.github.com>
2023-03-15 23:52:13 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const inputs = document.querySelector(".inputs"),
+hintTag = document.querySelector(".hint span"),
+guessLeft = document.querySelector(".guess-left span"),
+wrongLetter = document.querySelector(".wrong-letter span"),
+resetBtn = document.querySelector(".reset-btn"),
+typingInput = document.querySelector(".typing-input");
+
+let word, maxGuesses, incorrectLetters = [], correctLetters = [];
+
+function randomWord() {
+    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    hintTag.innerText = ranItem.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
+randomWord();
+
+function initGame(e) {
+    let key = e.target.value.toLowerCase();
+    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if(word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if(word[i] == key) {
+                    correctLetters += key;
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        } else {
+            maxGuesses--;
+            incorrectLetters.push(` ${key}`);
+        }
+        guessLeft.innerText = maxGuesses;
+        wrongLetter.innerText = incorrectLetters;
+    }
+    typingInput.value = "";
+
+    setTimeout(() => {
+        if(correctLetters.length === word.length) {
+            alert(`¡Lo conseguiste padrina!`);
+            document.body.innerHTML = `<center><h1 style="color: white;">Antes de reclamar tu sorpresa, tendrás que resolver la siguiente adivinanza: "No muerde ni ladra, pero tiene dientes y la casa guarda." ¿Qué es? </h1> <h2 style="color: white;">Una vez tengas la respuesta ven a reclamar tu sorpresa</h2></center>`
+        } else if(maxGuesses < 1) {
+            alert("Se acabó el juego, no adivinaste pero sigue intentando :C");
+            window.location.reload()
+        }
+    }, 100);
+}
+
+resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
---
+++ js/words.js
@@ -0,0 +1,27 @@
+let wordList = [
+    {
+        word: "pijamada",
+        hint: "Lo que comenzó todo."
+    },
+    {
+        word: "crepas",
+        hint: "Una comida que nos encanta."
+    },
+    {
+        word: "jenga",
+        hint: "Un juego donde te vencí B)."
+    },
+    {
+        word: "billar",
+        hint: "El primer abrazo fue cuando te enseñaba a jugar..."
+    },
+    {
+        word: "atencion",
+        hint: "Si te puse..."
+    },
+    {
+        word: "rosa",
+        hint: "Y esta..."
+    },
+
+]
